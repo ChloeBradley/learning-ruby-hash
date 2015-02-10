e864dd24a9b459c4023ca38cee404ddddb5cb4f6
Chloe Bradley <ccbradley77@gmail.com>
2015-02-10 10:32:44 -0700

    commiting frist ruby hash tests

--- learning_hash.rb
+++ learning_hash.rb
@@ -2,15 +2,48 @@
 
 class LearningHash
   def foo_key(hash)
-    # your code here
+    hash[:foo]
+
   end
 
   def bar_key(hash)
-    # your code here
+    hash[:bar]
+
   end
 
   def key_stringer(hash)
-    # your code here
+    result = ''
+      hash.each_key do |key|
+        result << key.to_s
+        h.each
+      end
+      return result
+    end
+
+    def value_stringer(hash)
+      result = ''
+      hash.each do |key, value|
+        result = [result, value.to_s].join
+        #result += value.to_s
+      end
+
+      #hash .each_value do |value|
+      # result << value.to_so
+      #end
+
+      return result
+    end
+
+    def key_and_value_stringer(hash)
+      result = ''
+
+      hash.each_pair do |key, value|
+        result << key.to_s + value.to_s
+      end
+      result
+    end
+  end
+  
+
     # hint see http://ruby-doc.org/core-2.1.5/Hash.html#method-i-each_key
   end
-end
